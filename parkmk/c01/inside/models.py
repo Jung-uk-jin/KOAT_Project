c919a944f744a7268746905cb2155d5c4ec36a57
parkmk0615 <parkmk0615@naver.com>
2024-12-10 17:51:26 +0900

    1210,지역 백엔드

--- parkmk/c01/inside/models.py
+++ parkmk/c01/inside/models.py
@@ -2,17 +2,23 @@ from django.db import models
 from member.models import Member
 from datetime import datetime
 
+
 class Location_inform(models.Model):
   l_no = models.AutoField(primary_key=True)
   l_member = models.ForeignKey(Member,on_delete = models.DO_NOTHING,null=True)
   l_location = models.CharField(max_length=30)
   l_description = models.TextField(null=True)
-  l_description2 = models.CharField(max_length=3000, default="Default Description")
-  l_name =models.CharField(max_length=30)
-  l_file = models.ImageField(null=True,upload_to='board')
-
+  l_file = models.ImageField(null=True,upload_to='board/')
+  
   def __str__(self):
-    return f"{self.l_no},{self.l_description2},{self.l_location},{self.l_name}"
+    return f"{self.l_no},{self.l_location}"
 
 class Location(models.Model):
   lo_name = models.CharField(max_length=30, primary_key=True)
+
+
+class Attraction(models.Model):
+  a_location = models.ForeignKey(Location_inform, on_delete = models.CASCADE, null=True)
+  a_name = models.CharField(max_length=30, primary_key=True)
+  a_description = models.TextField(null=True)
+  a_file = models.ImageField(null=True,upload_to='board/')
--- parkmk/c01/inside/views.py
+++ parkmk/c01/inside/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from inside.models import Location_inform
-from inside.models import Location
+from inside.models import Attraction
 
-def up(request,lo_name):
-    qs = Location_inform.objects.get(l_location=lo_name)
+def up(request, lo_name):
+    # l_location 필드를 기준으로 필터링
+    qs = Attraction.objects.filter(a_location__l_location=lo_name)
+    qx = Location_inform.objects.filter(l_location=lo_name)
 
-    context = {'informs':qs}
-    return render(request, 'up.html',context)
+    context = {'informs': qs, 'location':qx}
+    return render(request, 'up.html', context)

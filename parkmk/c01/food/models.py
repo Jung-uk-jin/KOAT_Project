2332d402362c2ef329b901991204323adf13c7ae
parkmk0615 <parkmk0615@naver.com>
2024-12-12 18:30:08 +0900

    1212

--- parkmk/c01/food/models.py
+++ parkmk/c01/food/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from member.models import Member
+from phonenumber_field.modelfields import PhoneNumberField    # pip install django-phonenumber-field[phonenumbers]   <-- 설치 해야 됨
 
 class Food_inform(models.Model):
   f_no = models.AutoField(primary_key=True)
   f_member = models.ForeignKey(Member, on_delete=models.DO_NOTHING,null=True)
   f_name = models.CharField(max_length=10)
   f_description = models.TextField()
-  f_location = models.CharField(max_length=30)
   f_file = models.ImageField(null=True,upload_to='board')
 
   def __str__(self):
-    return f"{self.f_no},{self.f_name},{self.f_description},{self.f_location}"
+    return f"{self.f_no},{self.f_name},{self.f_description}"
+  
+class Eat(models.Model):
+  e_name = models.CharField(max_length=30, primary_key=True)
+
+
+class Store(models.Model):
+  s_name = models.CharField(max_length=30, primary_key=True)
+  s_address = models.CharField(max_length=100,null=True)
+  s_number = PhoneNumberField(null=True, blank=True, region='KR')  # 한국 기준 전화번호
+  s_file = models.ImageField(null=True,upload_to='board/')
--- parkmk/c01/food/views.py
+++ parkmk/c01/food/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from food.models import Food_inform
+from food.models import Store
 
 def eat(request):
   return render(request,'eat.html')
 
-def niku(request):
-  return render(request,'niku.html')
+def niku(request,e_name):
+  qs = Store.objects.filter(f_name=e_name)
+  qx = Food_inform.objects.filter(f_name=e_name)
+
+  context = {'informs' : qs,'location':qx}
+  return render(request,'niku.html',context)

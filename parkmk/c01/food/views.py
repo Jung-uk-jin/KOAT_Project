f6f50048f2432f8aee9c05d8c39b3cd486a77eec
parkmk0615 <parkmk0615@naver.com>
2024-12-13 14:31:06 +0900

    1213

--- parkmk/c01/food/views.py
+++ parkmk/c01/food/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from food.models import Food_inform
 from food.models import Store
+from food.models import Eat
+
 
 def eat(request):
-  return render(request,'eat.html')
+  qc = Eat.objects.all()
+  context={'flist':qc}
+  return render(request,'eat.html',context)
 
 def niku(request,e_name):
-  qs = Store.objects.filter(f_name=e_name)
+  qs = Store.objects.filter(s_foodname__f_name=e_name)
   qx = Food_inform.objects.filter(f_name=e_name)
 
   context = {'informs' : qs,'location':qx}
